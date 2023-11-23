11c60841284f9a08498f4697d7bce033ecfa6b9e
EKnight-Blue <loicchevalier@gmail.com>
2023-11-23 18:54:51 +0100

    ps

--- Loic/controller/test_read.py
+++ Loic/controller/test_read.py
@@ -1,5 +1,5 @@
-with open('/dev/input/js0') as js:
-    with open('/dev/input/mouse0') as ms:
+with open('/dev/input/js0', 'rb') as js:
+    with open('/dev/input/mouse0', 'rb') as ms:
         while True:
             print('Joysticks', js.read(5))
             print('Mouse', ms.read(5))
