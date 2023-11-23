19b2340a9494d10b2d3b0ff8d2f890ad511bad8b
EKnight-Blue <loicchevalier@gmail.com>
2023-11-23 18:04:28 +0100

    ps

--- Loic/controller/__init__.py
+++ Loic/controller/__init__.py
@@ -1,45 +1,118 @@
 from multiprocessing import Process, SimpleQueue
-from controller_classes import ControllerButtons, ControllerMouse
+from threading import Thread
+import asyncio
+
+from controller_classes import ControllerButtons, ControllerMouse, PROCESS, THREAD, ASYNCIO
 
 
-class Controller:
+class ControllerInterface:
     """
     Tool to get events from a ps4 controller, works on UNIX
     """
     
-    def __init__(self, joystick_file='/dev/input/js0', mouse_file='/dev/input/mouse0'):
-        self.queue = SimpleQueue()
-        self.controllerButtonsProcess = Process(target=ControllerButtons(self.queue, joystick_file).mainloop)
-        self.controllerMouseProcess = Process(target=ControllerMouse(self.queue, mouse_file).mainloop)
+    def __init__(self, queue, joystick_file='/dev/input/js0', mouse_file='/dev/input/mouse0'):
+        self.queue = queue
+        self.butts, self.mouse = ControllerButtons(queue, joystick_file), ControllerMouse(queue, mouse_file)
+
+    def get_events(self):
+        pass
+
+    def start(self):
+        pass
+
+    def terminate(self):
+        pass
+
+
+class CProcess(ControllerInterface):
+    butt_p, mouse_p = None, None
+
+    def start(self):
+        self.butt_p = Process(target=self.butts.mainloop_process)
+        self.mouse_p = Process(target=self.mouse.mainloop_process)
+        self.butt_p.start()
+        self.mouse_p.start()
 
     def get_events(self):
         while not self.queue.empty():
             yield self.queue.get()
 
+    def terminate(self):
+        self.butt_p.terminate()
+        self.mouse_p.terminate()
+
+
+class CThread(ControllerInterface):
+    butt_t, mouse_t = None, None
+
     def start(self):
-        self.controllerButtonsProcess.start()
-        self.controllerMouseProcess.start()
+        self.butt_t = Thread(target=self.butts.mainloop_thread)
+        self.mouse_t = Thread(target=self.mouse.mainloop_thread)
+        self.butt_t.start()
+        self.mouse_t.start()
+
+    def get_events(self):
+        while self.queue:
+            yield self.queue.pop(0)
 
     def terminate(self):
-        self.controllerMouseProcess.terminate()
-        self.controllerButtonsProcess.terminate()
+        self.butts.running = False
+        self.mouse.running = False
 
-    def is_alive(self):
-        return self.controllerMouseProcess.is_alive() or self.controllerButtonsProcess.is_alive()
+
+class CAsync(ControllerInterface):
+    butt_t, mouse_t = None, None
+
+    def get_events(self):
+        while self.queue:
+            yield self.queue.pop(0)
+
+
+def async_main():
+    controller = CAsync([])
+
+    async def display():
+        while True:
+            for event in controller.get_events():
+                print(event)
+            await asyncio.sleep(0.)
+
+    async def main():
+        await asyncio.gather(
+            display(),
+            controller.butts.mainloop_asyncio(),
+            controller.mouse.mainloop_asyncio()
+        )
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        controller.butts.running = False
+        controller.mouse.running = False
+
+
+def process_main():
+    controller = CProcess(SimpleQueue())
+    controller.start()
+    try:
+        while True:
+            for ev in controller.get_events():
+                print(ev)
+    except KeyboardInterrupt:
+        controller.terminate()
+
+
+def thread_main():
+    controller = CThread([])
+    controller.start()
+    try:
+        while True:
+            for ev in controller.get_events():
+                print(ev)
+    except KeyboardInterrupt:
+        controller.terminate()
 
 
 if __name__ == '__main__':
-    print("Creating controller")
-    c = Controller()
-    print("Controller created")
-    c.start()
-    print("Now listening")
-    try:
-        print('Listen')
-
-        while c.is_alive():
-            for ev in c.get_events():
-                print(ev)
-        print('Ended')
-    except KeyboardInterrupt:
-        c.terminate()
+    import sys
+    mode = sys.argv[1]
+    {'asyncio': async_main, 'thread': thread_main, 'process': process_main}[mode]()
--- Loic/controller/controller_classes.py
+++ Loic/controller/controller_classes.py
@@ -1,5 +1,6 @@
 import struct
 from constants import *
+import asyncio
 
 
 class Event:
@@ -13,43 +14,110 @@ class Event:
         return EVENT_FORMAT.format(EVENT_TYPES[self.type], BUTTONS.get((self.type, self.button), ""), self.value)
 
 
-class ControllerButtons:
+class ControllerInterface:
+    event_length: int
+    running = True
+
+    def read_process_thread(self, file):
+        return file.read(self.event_length)
+
+    async def read_asyncio(self, file):
+        return await asyncio.to_thread(file.read, self.event_length)
+
+
+class ControllerButtons(ControllerInterface):
     event_format = '3Bh2b'
     event_length = struct.calcsize(event_format)
 
     def __init__(self, queue, location):
         self.file, self.queue = location, queue
 
-    def mainloop(self):
+    def mainloop_process(self):
         try:
             with open(self.file, 'rb') as f:
                 while True:
                     # read will block, that's why i use processes
-                    self.queue.put(Event(*struct.unpack(self.event_format, f.read(self.event_length))[:2:-1]))
+                    self.queue.put(Event(*struct.unpack(self.event_format, self.read_process_thread(f)[:2:-1])))
+        except KeyboardInterrupt:
+            print('Terminated Controller Process')
+        except OSError:
+            print('Controller Disconnected')
+
+    async def mainloop_asyncio(self):
+        try:
+            with open(self.file, 'rb') as f:
+                while self.running:
+                    self.queue.append(Event(*struct.unpack(
+                        self.event_format, (await self.read_asyncio(f))[:2:-1]
+                    )))
+        except KeyboardInterrupt:
+            print('Terminated Controller Process')
+        except OSError:
+            print('Controller Disconnected')
+
+    def mainloop_thread(self):
+        try:
+            with open(self.file, 'rb') as f:
+                while self.running:
+                    self.queue.append(Event(*struct.unpack(
+                        self.event_format, self.read_process_thread(f)[:2:-1]
+                    )))
         except KeyboardInterrupt:
             print('Terminated Controller Process')
         except OSError:
             print('Controller Disconnected')
 
 
-class ControllerMouse:
+class ControllerMouse(ControllerInterface):
     event_length = 3
 
     def __init__(self, queue, location):
         self.file, self.queue = location, queue
         self.pad_state = 0
 
-    def mainloop(self):
+    def mainloop_process(self):
         try:
             with open(self.file, 'rb') as f:
                 while True:
                     # will block, that's why i use processes
-                    h, dx, dy = f.read(self.event_length)
+                    h, dx, dy = self.read_process_thread(f)
                     if (h & 1) ^ self.pad_state:
                         self.pad_state = h & 1
                         self.queue.put(Event(h & 1, 3, None))
                     # Two's complement on x and y displacements
                     self.queue.put(Event(h & 1, 3, ((dx - 256 if dx > 128 else dx), dy - 256 if dy > 128 else dy)))
+        except KeyboardInterrupt:
+            print('Terminated Mouse Process')
+        except OSError:
+            print('Controller Disconnected')
+
+    def mainloop_thread(self):
+        try:
+            with open(self.file, 'rb') as f:
+                while self.running:
+                    # will block, that's why i use processes
+                    h, dx, dy = self.read_process_thread(f)
+                    if (h & 1) ^ self.pad_state:
+                        self.pad_state = h & 1
+                        self.queue.append(Event(h & 1, 3, None))
+                    # Two's complement on x and y displacements
+                    self.queue.append(Event(h & 1, 3, ((dx - 256 if dx > 128 else dx), dy - 256 if dy > 128 else dy)))
+        except KeyboardInterrupt:
+            print('Terminated Mouse Process')
+        except OSError:
+            print('Controller Disconnected')
+
+    async def mainloop_asyncio(self):
+        try:
+            with open(self.file, 'rb') as f:
+                while self.running:
+                    # will block, that's why i use processes
+                    h, dx, dy = await self.read_asyncio(f)
+                    if (h & 1) ^ self.pad_state:
+                        self.pad_state = h & 1
+                        self.queue.append(Event(h & 1, 3, None))
+                    # Two's complement on x and y displacements
+                    self.queue.append(Event(h & 1, 3, ((dx - 256 if dx > 128 else dx), dy - 256 if dy > 128 else dy)))
         except KeyboardInterrupt:
             print('Terminated Mouse Process')
         except OSError:
