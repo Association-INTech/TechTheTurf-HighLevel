57ee7df83853e3e18502849930221fa3926d9476
EKnight-Blue <loicchevalier@gmail.com>
2023-11-23 23:08:54 +0100

    ps

--- Loic/just_print_sinus.py
+++ Loic/just_print_sinus.py
@@ -4,10 +4,10 @@ import struct
 import sys
 
 
-sys.stout.write('i am sending floating point values\n')
 time.sleep(.5)
 duration, date = float(sys.argv[1]), time.time()
 
+sys.stdout.write('i am sending floating point values\n')
 while time.time() - date < duration:
     # print('\r', f'{time.time() - date:.04f}', end='')
     sys.stdout.buffer.write(struct.pack('f', math.sin(time.perf_counter() / 1e2)))
