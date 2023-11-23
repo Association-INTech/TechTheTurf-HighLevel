7f8a5f3f1c1d3c1d1ac61cb29a683e73a47c38f0
EKnight-Blue <loicchevalier@gmail.com>
2023-11-23 22:16:27 +0100

    ps

--- Loic/just_print_sinus.py
+++ Loic/just_print_sinus.py
@@ -4,9 +4,11 @@ import struct
 import sys
 
 
-duration, date = float(sys.argv[1]), time.perf_counter()
+duration, date = float(sys.argv[1]), time.time()
 
-while time.perf_counter() - date < duration:
-    print('\r', time.perf_counter() - date, end='')
+while time.time() - date < duration:
+    print('\r', time.time() - date, end='')
     # sys.stdout.buffer.write(struct.pack('f', math.sin(time.perf_counter() / 1e6)))
     # sys.stdout.flush()
+
+print()
