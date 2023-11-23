3815ce8a41e26f55c96460ec97cf23d429cc6a96
djpadbit <djpadbit@gmail.com>
2023-11-23 20:32:14 +0100

    Use custom I2C send/receive
    Signed-off-by: djpadbit <djpadbit@gmail.com>

--- Pablo/pami.py
+++ Pablo/pami.py
@@ -32,6 +32,16 @@ class Pid:
 	def __str__(self):
 		return f"Pid(name={self.name}, kp={self.kp}, ki={self.ki}, kd={self.kd})"
 
+def write_i2c(bus, addr, reg, data):
+	write = smbus2.i2c_msg.write(addr, struct.pack("<B", reg) + data)
+	bus.i2c_rdwr(write)
+
+def read_i2c(bus, addr, reg, size):
+	write = smbus2.i2c_msg.write(addr, struct.pack("<B", reg))
+	bus.i2c_rdwr(write)
+	read = smbus2.i2c_msg.read(addr, size)
+	bus.i2c_rdwr(read)
+	return bytes(read)
 
 class Pami(cmd.Cmd):
 	def __init__(self, addr):
@@ -43,14 +53,14 @@ class Pami(cmd.Cmd):
 
 	def do_on(self, arg):
 		"""Allume le Pami"""
-		bus.write_i2c_block_data(self.addr, 0, struct.pack('<B', 1))
+		write_i2c(bus, self.addr, 0, struct.pack('<B', 1))
 
 		self.started = True
 		print("Pami - ON")
 
 	def do_off(self, arg):
 		"""éteint le pami, le stoppe et remet ses roues droites"""
-		bus.write_i2c_block_data(self.addr, 0, struct.pack('<B', 0))
+		write_i2c(bus, self.addr, 0, struct.pack('<B', 0))
 
 		self.started = False
 		print("Pami - OFF")
@@ -63,7 +73,7 @@ class Pami(cmd.Cmd):
 
 	def do_pos(self, arg):
 		"""demande la position et l'angle teta d'orientation du robot"""
-		res = bus.read_i2c_block_data(self.addr, 3, 2*4)
+		res = read_i2c(bus, self.addr, 3, 2*4)
 		x,y = struct.unpack("<ff", res)
 		print(f"x: {x}, y:{y}")
 
@@ -79,7 +89,7 @@ class Pami(cmd.Cmd):
 
 		theta, dst = map(float,arg.split())
 
-		bus.write_i2c_block_data(self.addr, 1, struct.pack('<ff', dst, theta))
+		write_i2c(bus, self.addr, 1, struct.pack('<ff', dst, theta))
 		print(f"Déplacement de theta {theta} et rho {dst}")
 
 	def do_gpid(self, arg):
@@ -100,7 +110,7 @@ class Pami(cmd.Cmd):
 				print("Pas bon Pid")
 				return
 
-		res = bus.read_i2c_block_data(self.addr, 2 | (idx << 4), 4*3)
+		res = read_i2c(bus, self.addr, 2 | (idx << 4), 4*3)
 		self.pids[idx].from_bytes(res)
 		print(self.pids[idx])
 
@@ -123,10 +133,10 @@ class Pami(cmd.Cmd):
 				print("Pas bon Pid")
 				return
 
-		kp, ki, kd = map(float,arg[1:])
+		kp, ki, kd = map(float, arg[1:])
 		self.pids[idx].set(kp, ki, kd)
-
-		res = bus.write_i2c_block_data(self.addr, 5 | (idx << 4), self.pids[idx].to_bytes())
+		print(self.pids[idx])
+		write_i2c(bus, self.addr, 5 | (idx << 4), self.pids[idx].to_bytes())
 
 if __name__ == "__main__":
 	pami = Pami(PAMI_I2C_ADDR)
