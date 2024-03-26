66c42a26be570ee8203c34ef99f1872fdad34eea
djpadbit <djpadbit@gmail.com>
2024-03-26 16:04:59 +0100

    Added jumper and put main back to square scenario.
    Signed-off-by: djpadbit <djpadbit@gmail.com>

--- main.py
+++ main.py
@@ -1,36 +1,46 @@
-import time
-import math
+import time, math
+import RPi.GPIO as GPIO
 
 import comm
+
+JUMPER_PIN = 22
+SAFE = True
+
+GPIO.setmode(GPIO.BCM)
+# Jumper in input Pull-Up
+GPIO.setup(JUMPER_PIN, GPIO.IN, GPIO.PUD_UP)
+
+def wait_for_jumper():
+	if SAFE and GPIO.input(JUMPER_PIN):
+		print("Waiting for jumper to be inserted...")
+
+		while GPIO.input(JUMPER_PIN):
+			time.sleep(0.01)
+
+		time.sleep(1)
+
+	print("Waiting for start...")
+
+	while not GPIO.input(JUMPER_PIN):
+		time.sleep(0.01)
+
+	print("Starting")
 
 # Example scenario
 
 asserv = comm.make_asserv()
 
-asserv.set_telem(asserv.telem_from_idx(0), True)
-asserv.set_telem(asserv.telem_from_idx(1), True)
-asserv.set_telem(asserv.telem_from_idx(2), True)
-asserv.set_telem(asserv.telem_from_idx(3), True)
-
 asserv.start()
+
+wait_for_jumper()
 
 side_len = 500 # mm
 
-print("move")
-asserv.move(1000, 0, blocking=False)
-time.sleep(1)
-print("estop")
-asserv.emergency_stop()
-time.sleep(1)
-print("balls")
-
-"""
 for i in range(4):
 	print(i)
 	asserv.move(side_len, 0)
 	time.sleep(2)
 	asserv.move(0, math.radians(90))
 	time.sleep(2)
-"""
 
 asserv.stop()
