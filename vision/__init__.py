2e58e540f73abce0bbe2b34539ae6d529078a085
Loic Chevalier <loicchechevalier@gmail.com>
2024-04-21 13:43:26 +0200

    Filter warnings

--- vision/__init__.py
+++ vision/__init__.py
@@ -1,3 +1,7 @@
 from .aruco import detect
 from .camera import Camera
 
+import warnings
+# TODO: comment this part to debug
+# SHUT UP NUMPY, let me divide by 0, just put those 'NaN' quietly; opencv may shut up too... good luck
+warnings.filterwarnings('ignore')
